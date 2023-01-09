4ce5d96c86048c5edeef648a3c83df6a939dbb9d
fidel boix <fidelboix@iesmre.com>
2023-01-09 18:02:59 +0100

    Primer commit, copia del ejercicio

---
+++ copia_01.java
@@ -0,0 +1,24 @@
+package ud04;
+
+import javax.swing.JOptionPane;
+
+public class _01Estaturas {
+    public static void main(String[] args) {
+        int cantidad;
+        int i=0;
+        String valores="";
+
+        cantidad=Integer.parseInt(JOptionPane.showInputDialog("Cuantas estaturas vas a introducir?"));
+
+        double estatura[] = new double[cantidad];
+
+        while(i<cantidad){
+            estatura[i]=Double.parseDouble(JOptionPane.showInputDialog(null, "Introduce la estatura "+(i+1)+":"));
+            i++;
+        }
+
+        for(int j=0; j<cantidad;j++){
+            valores+="Persona "+(j+1)+": "+estatura[j]+" m\n";
+        } JOptionPane.showMessageDialog(null, valores);
+    }
+}
